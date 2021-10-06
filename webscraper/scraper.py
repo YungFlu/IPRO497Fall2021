792688df9ac1474469b775178871f5f5413ca36d
amoizuddin <amoizuddin@hawk.iit.edu>
2021-10-06 01:17:19 -0500

    Created README.md and added functionality to scraper.py

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,1 +1,33 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+link = "http://bulletin.iit.edu/courses/"
+domain = "http://bulletin.iit.edu"
+result  = requests.get(link)
+
+print(result.status_code)
+
+src = result.content
+soup = BeautifulSoup(src, features="html.parser")
+
+##helper function
+def get_class_code(class_name):
+    start_idx = class_name.index('(')
+    return class_name[start_idx+1:len(class_name)-1]
+
+
+idx = soup.find("div", {"id":"atozindex"})
+dpts = idx.find_all('a', href=True)
+dpt_endpoints={}
+for dpt in dpts:
+    dpt_endpoints.update({get_class_code(dpt.text):dpt['href']})
+#print(dpt_endpoint)
+
+print(dpt_endpoints)
+
+'''
+we got all the departments and the endpoints to their course catalogs.
+now, to store the courses themselves
+create another dictonary, where keys are dpt names and vals are the course numbers. 
+'''
+
