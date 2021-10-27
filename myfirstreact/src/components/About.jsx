f7b9a2148327711eb14715c04a6664fd8315cc2a
YungFlu <lufranklin123@yahoo.com>
2021-10-26 19:09:25 -0500

    About page

--- myfirstreact/src/components/About.jsx
+++ myfirstreact/src/components/About.jsx
@@ -8,17 +8,14 @@ function About() {
           <div class="col-lg-7">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
+              src="https://media.istockphoto.com/photos/happy-college-students-talking-on-a-class-in-the-classroom-picture-id1161093572?k=20&m=1161093572&s=170667a&w=0&h=3n3Igsb5kxs4YeI12pQKZif0ItGXbt8ShRjCtticix0="
               alt=""
             />
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">About</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              This is a web app for IIT students that allows you to see comments on certain classes from IIT from previous students
             </p>
           </div>
         </div>
--- myfirstreact/src/components/Class.jsx
+++ myfirstreact/src/components/Class.jsx
@@ -19,21 +19,6 @@ class Class extends Component {
     console.log(this.props.location.state)
   }  
 
-  /*
-  renderClassName = () => {
-    console.log("I AM IN RENDERCLASSNAME")
-    if (this.props.location.state.dept != null && this.props.location.state.course != null){
-      document.getElementById("Course Title").innerHTML = this.props.location.state.dept + this.props.location.state.course;
-      this.state.class = this.props.location.state.dept + this.props.location.state.course;
-      console.log("renderClassName1: " + this.props.location.state.dept + this.props.location.state.course)
-    }else{
-      document.getElementById("Course Title").innerHTML = localStorage.getItem("Class")
-      this.state.class = localStorage.getItem("Class")
-      console.log("renderClassName2: " + localStorage.getItem("Class"))
-    }
-  }
-  */
-
   renderPost = () => {
     this.refreshPosts();
     return this.state.postList.map(Posts => (
@@ -51,7 +36,6 @@ class Class extends Component {
       </div>
     ));
   };
-
   
   refreshClasses = () => {
     axios
@@ -76,9 +60,6 @@ class Class extends Component {
     this.state.postComment = event.target.value
   }
 
-  
-  
-
   handlePost = Posts => {
     this.setState({class: this.state.class})
     console.log("handlePost: " + this.state.name + "- " + this.state.postComment)
@@ -86,7 +67,7 @@ class Class extends Component {
     axios
       .post("http://localhost:8000/api/Posts/", 
       {
-        id: Math.floor(Math.random() * 10000),
+        id: Math.floor(Math.random() * 10000 + 2),
         name: String(this.state.name),
         contents: String(this.state.postComment),
         Classes: 1, //this.state.class
@@ -107,18 +88,19 @@ class Class extends Component {
           <div class="col-lg-5">
             <h1 id="Course Title" class="font-weight-light">{this.state.class}</h1> 
               <div>
-                <form onSubmit={this.handlePost}>
+                  <form onSubmit={this.handlePost}>
 
-                  <label> 
-                    <div>
-                    <input type="text" value={this.state.value} onChange={this.handleName} />
-                    </div>
-                    <div>
-                    <input type="text" value={this.state.value} onChange={this.handleComment} />
-                    </div>
-                  </label>
-                    <input type="submit" value="Submit" />
-                </form>
+                    <label> 
+                      <div>
+                      <input type="text" value={this.state.value} onChange={this.handleName} />
+                      </div>
+                      <div>
+                      <input type="text" value={this.state.value} onChange={this.handleComment} />
+                      </div>
+                    </label>
+                      <input type="submit" value="Submit" />
+                  </form>
+                
               </div>
               <div >
               {this.renderPost()}
